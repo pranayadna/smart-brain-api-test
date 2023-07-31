49ea94ac04cb876862dd1d1029876bcd1772f9b0
pranayadna <pranayadnaap96@gmail.com>
2023-07-31 09:06:20 +0700

    editing image.js

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai. 
 const app = new Clarifai.App({
- apiKey: 'YOUR API KEY HERE' 
+ apiKey: '1ad5ea36436a4afb860f97199a2b26d8' 
 });
 
 
