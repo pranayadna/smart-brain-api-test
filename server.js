9d0e38ca1d897cde715bcc6122744749dc8a0fd9
pranayadna <pranayadnaap96@gmail.com>
2023-07-31 08:39:36 +0700

    adding db

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = knex({
   // connect to your own database here:
   client: 'pg',
   connection: {
-    connectionString : 'postgresql://postgres:9C8rpUm11fWMEKL7qrSN@containers-us-west-194.railway.app:6422/railway',
+    connectionString : 'postgresql://postgres:xB73KYPvzpLpyxdlCLSv@containers-us-west-194.railway.app:6422/railway',
     ssl: true
   }
 });
